dd020d2265bce3cccd4a960dc3881dd3e54837d2
UMA <the-smiling-moon-r62@ezweb.ne.jp>
2017-03-07 05:49:35 +0900

    edit .gitignore

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-#file      : main.py
-#date      : 17/02/12
-#author    : mi-na
+##
+# @file      : main.py
+# @date      : 17/02/12
+# @author    : mi-na
 
 
-#my module
+# my module
 from src.init import initialize
 import src.params as params
 import src.operate as ope
 from src.Logger import Logger
 
-#public module
+# public module
+# nothing
 
 
-#main process
+##! main process
 if __name__ == '__main__':
-    #initialize
+    ## @brief initialize
     g_map, g_players = initialize()
     g_map.printMap()
     print("turn 0")
@@ -22,7 +24,7 @@ if __name__ == '__main__':
         p.printPlayer()
     g_logger = Logger(g_players)
 
-    #routine
+    ## @brief routine
     status_code = "NEXT"
     for i in range(params.max_turn()):
         status_code, g_players, g_logger = ope.routine(i+1, g_players, g_map, g_logger)
